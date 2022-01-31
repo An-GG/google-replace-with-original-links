c5dbf6f41fc85af19fb34b76fdcded49b53fe91c
An-GG <ankush.girotra@gmail.com>
2022-01-31 09:00:24 -0600

    dist/img

--- index.ts
+++ index.ts
@@ -1,3 +1,28 @@
+// ==UserScript==
+// @name         google-replace-with-original-links
+// @namespace    https://www.returnyoutubedislike.com/
+// @homepage     https://github.com/An-GG/google-replace-with-original-links/
+// @version      0.0.1
+// @encoding     utf-8
+// @description  This script will observe for changes in the DOM tree and will swap out any tracking links if it sees them.
+// @icon         https://github.com/Anarios/return-youtube-dislike/raw/main/Icons/Return%20Youtube%20Dislike%20-%20Transparent.png
+// @author       Anarios & JRWR
+// @match        *://*.youtube.com/*
+// @exclude      *://music.youtube.com/*
+// @exclude      *://*.music.youtube.com/*
+// @compatible   chrome
+// @compatible   firefox
+// @compatible   opera
+// @compatible   safari
+// @compatible   edge
+// @downloadURL  https://raw.githubusercontent.com/An-GG/google-replace-with-original-links/master/dist/index.js
+// @updateURL    https://raw.githubusercontent.com/An-GG/google-replace-with-original-links/master/dist/index.js
+// @grant        GM.xmlHttpRequest
+// @connect      youtube.com
+// @grant        GM_addStyle
+// @run-at       document-end
+// ==/UserScript==
+
 function link_search() {
     for (let g of document.body.getElementsByClassName('g')) {
         for (let a of g.getElementsByTagName('a')) {
